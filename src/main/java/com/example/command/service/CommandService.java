8faa36a6163ebe0b3511d6352fba61ebc0feeb8c
Damien <fsutnovax@gmail.com>
2022-06-22 10:59:53 +0200

    ecriture fichiers / apply terraform

--- src/main/java/com/example/command/service/CommandService.java
+++ src/main/java/com/example/command/service/CommandService.java
@@ -20,44 +20,158 @@ public class CommandService {
     public CommandService(){
     }
 
+
+    private String providerStr = 'provider "aws" {
+                            profile    = "default"
+                            region     = "eu-west-3"
+                       }';
+    private String deployStr = '#####VPC#########################################
+                        resource "aws_vpc" "devvpc" {
+                            cidr_block = "192.168.0.0/16"
+                            tags = {
+                                Name = "dev_env"
+                            }
+                        }
+
+                        resource "aws_subnet" "devsub" {
+                            vpc_id	= aws_vpc.devvpc.id
+                            cidr_block	= "192.168.10.0/24"
+                            tags = {
+                                name = "sub_dev"
+                            }
+                        }
+
+                        resource "aws_internet_gateway" "dev_ig" {
+                            vpc_id = aws_vpc.devvpc.id
+                        }
+
+                        ###################################Security_group######################################
+
+                        resource "aws_security_group" "devsg" {
+                                vpc_id  = aws_vpc.devvpc.id
+
+                                ingress {
+                                        from_port       = 22
+                                        to_port         = 22
+                                        protocol        = "tcp"
+                                        cidr_blocks     = ["81.251.137.52/32"]
+                                }
+
+                            dynamic "ingress" {
+                            for_each = var.sg_ingress
+                            content {
+                            description      = lookup(ingress.value, "description", null)
+                            from_port        = lookup(ingress.value, "from_port", null)
+                            to_port          = lookup(ingress.value, "to_port", null)
+                            protocol         = lookup(ingress.value, "protocol", null)
+                            cidr_blocks      = lookup(ingress.value, "cidr_blocks", null)
+                            }
+                        }
+
+
+                                egress {
+                                        from_port       = 0
+                                        to_port         = 0
+                                        protocol        = "-1"
+                                        cidr_blocks     = ["0.0.0.0/0"]
+                                }
+                            tags = {
+                                Name = "devsg"
+                            }
+
+                        }
+                        #####################EIP#####################################
+                        resource "aws_eip" "deveip" {
+                            instance = aws_instance.dev_ec2.id
+                            vpc	= true
+                        }
+                        resource "aws_key_pair" "dev_key" {
+                        key_name   = "dev-key"
+                        public_key = "%s"
+                        }
+
+                        resource "aws_instance" "dev_ec2" {
+                                ami     = "ami-0c6ebbd55ab05f070"
+                                instance_type   = "t2.micro"
+                                key_name = "dev-key"
+                                subnet_id = aws_subnet.devsub.id
+                                vpc_security_group_ids  = [aws_security_group.devsg.id]
+                                tags = {
+                                        Name = "%s"
+                                }
+                        }';
+        private String variableStr = 'variable "sg_ingress" {
+                                default     = {
+                                    "my ingress rule" = {
+                                    "description" = "For SSH"
+                                    "from_port"   = "22"
+                                    "to_port"     = "22"
+                                    "protocol"    = "tcp"
+                                    "cidr_blocks" = ["%s/32"]
+                                    }
+                                }
+                                        type        = map(any)
+                                        description = "Security group rules"
+                                }';
+
+
+
     public ResultCommand shellCommand(String command) throws Exception {
         try {
-            File myFile = new File("/tmp/terraform.tf");
-            FileWriter myWriter = new FileWriter("/tmp/terraform.tf");
+            File provider = new File("/tmp/provider.tf");
+            File deploy = new File("/tmp/deploy.tf");
+            File variable = new File ("/tmp/variable.tf");
 
-            // Vérif
-            if(myFile.createNewFile()) {
-                // todo dto
-                logger.debug("File created: " + "myObj.getName()");
-                // fix me
-//                myWriter.write("Le Terraform ici %s","ip ici");
-                myWriter.close();
-                logger.debug("Successfully wrote to the file.");
+            Filewriter providerWriter = new FileWriter("/tmp/provider.tf");
+            Filewriter deployWriter = new FileWriter("/tmp/deploy.tf");
+            Filewriter variableWriter = new FileWriter("/tmp/variable.tf");
 
-            } else {
-                logger.debug("File already exists.");
-//                myWriter.write("Le Terraform ici %s","ip ici");
-                myWriter.close();
-                logger.debug("Successfully wrote to the file.");
-
+            if(!provider.exists()) {
+                provider.createNewFile();
+                providerWriter.write(this.providerStr);
+                providerWriter.close();
             }
 
-            // Execution commande terraform
+            if(!deploy.exists()) {
+                deploy.createNewFile();
+                deployWriter.write(this.deployStr, "key","nomMachine");
+                deployWriter.close();               
+            }
 
-            logger.info("command from shell");
+            if(!variable.exists()) {
+                variable.createNewFile();
+                variableWriter.write(this.variableStr, "IP");
+                variableWriter.close();               
+            }
+
+
+            // Commandes Terraforms
+
+            logger.info("terraform init");
 
             boolean isWindows = System.getProperty("os.name")
-                    .toLowerCase().startsWith("windows");
+            .toLowerCase().startsWith("windows");
 
             String homeDirectory = System.getProperty("user.home");
-            Process process = null;
+            
             if (isWindows) {
                 process = Runtime.getRuntime()
-                        .exec(String.format("cmd.exe /c %s", command));
+                        .exec(String.format("cd /tmp/ || terraform init -force-copy"))
+                        .exec(String.format("cd /tmp/ || terraform apply -auto-approve"));
             } else {
                 process = Runtime.getRuntime()
-                        .exec(String.format("sh -c %s", command));
+                        .exec(String.format("cd /tmp/ || terraform init -force-copy"))
+                        .exec(String.format("cd /tmp/ || terraform apply  -auto-approve"));
             }
+
+
+
+
+
+
+            
+            Process process = null;
+
 
             BufferedReader stdInput = new BufferedReader(new
                     InputStreamReader(process.getInputStream()));
@@ -87,6 +201,23 @@ public class CommandService {
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
+            
+            // Suppression fichiers
+            if(deploy.exists()) {
+                deploy.delete();
+                logger.info("/tmp/deploy.tf deleted");
+
+            }
+            if(provider.exists()) {
+                provider.delete();
+                logger.info("/tmp/provider.tf deleted");
+
+            }
+            if(variable.exists()) {
+                variable.delete();
+                logger.info("/tmp/variable.tf deleted");
+
+            }
             logger.info("closing connection");
         }
 
