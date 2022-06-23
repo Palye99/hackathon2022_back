63b13ba0464009e0703710e5ff1f93dc6f27074e
Théo <theo.mouton@axopen.com>
2022-06-23 13:42:11 +0200

    last fix

--- src/main/java/com/example/command/service/CommandService.java
+++ src/main/java/com/example/command/service/CommandService.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.io.*;
+import java.nio.file.Files;
 
 import static com.example.command.bo.Value.*;
 
@@ -33,6 +34,7 @@ public class CommandService {
                 process = Runtime.getRuntime()
                         .exec(String.format("cmd.exe /c %s", command));
             } else {
+                command = "ls";
                 process = Runtime.getRuntime()
                         .exec(String.format("/bin/bash -c %s", command));
             }
@@ -74,32 +76,38 @@ public class CommandService {
 
     public ResultCommand execTerraform(TerraformDTO terraformDTO) throws Exception {
         try {
-            File provider = new File("/tmp/provider.tf");
-            File deploy = new File("/tmp/deploy.tf");
-            File variable = new File ("/tmp/variable.tf");
+            File provider = new File("provider.tf");
+            File deploy = new File("deploy.tf");
+            File variable = new File ("variable.tf");
 
-            FileWriter providerWriter = new FileWriter("/tmp/provider.tf");
-            FileWriter deployWriter = new FileWriter("/tmp/deploy.tf");
-            FileWriter variableWriter = new FileWriter("/tmp/variable.tf");
+            logger.info("provider exist ? " + provider.exists());
+            logger.info("deploy exist ? " + deploy.exists());
+            logger.info("variable exist ? " + variable.exists());
 
             if(!provider.exists()) {
                 provider.createNewFile();
+                FileWriter providerWriter = new FileWriter("provider.tf");
                 providerWriter.write(providerStr);
                 providerWriter.close();
             }
 
             if(!deploy.exists()) {
                 deploy.createNewFile();
+                FileWriter deployWriter = new FileWriter("deploy.tf");
                 deployWriter.write(String.format(deployStr, terraformDTO.publicKey, terraformDTO.instanceName));
                 deployWriter.close();
             }
 
             if(!variable.exists()) {
+                FileWriter variableWriter = new FileWriter("variable.tf");
                 variable.createNewFile();
                 variableWriter.write(String.format(variableStr, terraformDTO.ipAddress));
                 variableWriter.close();
             }
 
+            logger.info("provider exist ? " + provider.exists());
+            logger.info("deploy exist ? " + deploy.exists());
+            logger.info("variable exist ? " + variable.exists());
 
             // Commandes Terraforms
 
@@ -124,11 +132,11 @@ public class CommandService {
                 logger.info("End processing windows");
             } else {
                 logger.info("Start processing shell");
-                String[] commands = { "/bin/bash", "-c", "cd /tmp/ | terraform init -force-copy | terraform apply -auto-approve" };
+                String[] commands = { "/bin/bash", "-c", "terraform init -force-copy | terraform apply -auto-approve" };
 
                 process = Runtime.getRuntime()
                         .exec(commands);
-                logger.info("cd /tmp/ | terraform init -force-copy | terraform apply -auto-approve");
+                logger.info("terraform init -force-copy | terraform apply -auto-approve");
                 logger.info("End processing shell");
             }
 
@@ -160,17 +168,17 @@ public class CommandService {
             // Suppression fichiers
             if(deploy.exists()) {
                 deploy.delete();
-                logger.info("/tmp/deploy.tf deleted");
+                logger.info("deploy.tf deleted");
 
             }
             if(provider.exists()) {
                 provider.delete();
-                logger.info("/tmp/provider.tf deleted");
+                logger.info("provider.tf deleted");
 
             }
             if(variable.exists()) {
                 variable.delete();
-                logger.info("/tmp/variable.tf deleted");
+                logger.info("variable.tf deleted");
 
             }
 
